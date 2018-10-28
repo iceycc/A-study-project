069f4269ebed4f28a10a21a8a6dd28b5dcb847d1
iceyang <w@icey.cc>
2018-10-28 19:40:17 +0800

    add react-redux

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from "react";
+// import {render} from "react-dom"
+// import App from "./App"
+// render(<App/>,window.root)
+// document.write('hello')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let state = {
+  title : {
+    color:"red",
+    text:"标题",
+  },
+  content:{
+    color:"green",
+    text:"内容"
+  }
+}
+function renderTitle(){
+  let title = document.getElementById('title');
+  title.style.color = state.title.color;
+  title.innerHTML = state.title.text
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function renderContent(){
+  let content = document.getElementById('content');
+  content.style.color = state.content.color;
+  content.innerHTML = state.content.text
+}
+function render(){
+  renderTitle();
+  renderContent();
+}
+
+render()
+
+function dispatch(action){
+  switch (action.type){
+    case "CHANGE_TITLE_COLOR":
+      state = {...state,title:{...state.title,color:action.color}}
+      break;
+    case "CHANGE_CONTENT_COLOR":
+      state = {...state,content:{...state.content,text:action.text}}
+      break;
+  }
+}
+
+// 尽可能把状态保护起来
+setTimeout(()=>{
+  dispatch({
+    type:'CHANGE_TITLE_COLOR',
+    color:'blue'
+  })
+  dispatch({
+    type:'CHANGE_CONTENT_COLOR',
+    text:'action-内容'
+  })
+  render()
+},1000)
